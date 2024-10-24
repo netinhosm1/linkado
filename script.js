a4e7c917865690b82e4d38539e2382cbc10ba9b3
Seu Nome <n.netodev@gmail.comgit config usar.name 'Neto silva>
2024-10-23 21:11:11 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  /* if(html.classList.contains('light')){
+    html.classList.remove('light')
+  }
+  else {
+    html.classList.add('light')
+  }*/
+  html.classList.toggle("light") // faz a mesma coisa que o condicional acima
+
+  // trocar a tag img e substituir a imagem quando tiver em light ou dark
+
+  //seleciona a tag img no html e salva em img
+  const img = document.querySelector("#profile img")
+  //se estiver em light
+  if (html.classList.contains("light")) {
+    //adiciona a imagem light a tag img
+    img.setAttribute("src", "./assets/assets/avatar-light.png")
+    img.setAttribute("alt", "Foto de Perfil light")
+  } else {
+    //se estiver em dark, adiciona a imagem dark a tag img
+    img.setAttribute("src", "./assets/assets/avatar.png")
+    img.setAttribute("alt", "Foto de Perfil Dark")
+  }
+}
